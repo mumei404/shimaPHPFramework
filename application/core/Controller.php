d8e4a4b19df2492301cf141845aff867fcbda6b7
shimazaki <lozld30@gmail.com>
2019-06-22 00:57:49 +0900

    create redirect

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -52,4 +52,23 @@ abstract class Controller
 		$path = $this->controller_name . '/' . $template;
 		return $view->render($path, $variables, $layouta);
 	}
+
+	protected function forward404()
+	{
+		throw new HttpNotFoundException('Forwarded 404 page from ' . $this->controller_name . '/' . $this->action_name);
+	}
+
+	protected function redirect($url)
+	{
+		if(!preg_match('#http?://#', $url)) {
+			$protocol = $this->request->isSsl() ? 'https://' : 'http://';
+			$host = $this-request->getHost();
+			$base_url = $this->request->getBaseUrl();
+
+			$url = $protocol . $host . $base_url . $url;
+		}
+
+		$this->response->setStatusCode(302, 'Found');
+		$this->response->setHttpHeader('Location', $url);
+	}
 }
